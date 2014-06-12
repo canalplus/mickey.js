9d84c720b86832a0ac00bd4e4e8357b94b330c47
Pierre Guilleminot <pierre.guilleminot@gmail.com>
2014-06-12 14:14:35 +0200

    wip react.js example

---
+++ example/react/app.js
@@ -0,0 +1,24 @@
+(function() {
+
+  var ScrollDown = React.createClass({
+    getInitialState: function() {
+      return { from: 0, length: 10 };
+    },
+    render: function() {
+      var up = this.state.from;
+      var to = this.state.from + this.state.length;
+      return React.DOM.ul(null, _.range(up, to).map(function(i) {
+        return React.DOM.li({ className: 'item', onClick: _.bind(this.onClick, this) }, 'item' + i);
+      }, this));
+    },
+    onClick: function() {
+      this.setState({ from: this.state.from + 1 });
+    }
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    React.renderComponent(ScrollDown(), document.getElementById('main'));
+    new Mouse(document.getElementById('main')).init();
+  });
+
+})();
