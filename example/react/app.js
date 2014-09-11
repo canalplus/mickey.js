d1fc414332db5ad1640e545e163498db8e06bd6c
Pierre Guilleminot <pierre.guilleminot@gmail.com>
2014-09-12 00:33:43 +0200

    improve react example

--- example/react/app.js
+++ example/react/app.js
@@ -1,42 +1,88 @@
+/** @jsx d */
 (function(React, Mickey) {
 
   var d = React.DOM;
-  var mouse;
+  var cx = React.addons.classSet;
+
+  var MouseUpdateMixin = {
+    componentDidUpdate() {
+      mouse.update();
+    }
+  };
+
+  var MouseInitMixin = {
+    componentDidMount() {
+      mouse.init();
+    }
+  };
 
   var App = React.createClass({
-    render: function() {
-      return d.div(null, [
-        d.h1(null, "foo"),
-        ScrollDown({ length: 15 })
-      ]);
-    }
+    mixins: [MouseInitMixin],
+    getInitialState() {
+      return { selected: [] };
+    },
+    onSelect(obj) {
+      var prev = this.state.selected;
+      var find = _.find(prev, obj);
+      var next = find
+        ? _.without(prev, find)
+        : _.flatten([prev, obj]);
+      this.setState({ selected: next });
+    },
+    render() {
+      return (
+        <div>
+          <h1>{this.props.title}</h1>
+          <ScrollDown length={10} values={this.props.values} selected={this.state.selected} onSelect={(x) => this.onSelect(x)}/>
+          <SelectedItems values={this.state.selected}/>
+        </div>
+      )
+    },
   });
 
   var ScrollDown = React.createClass({
-    getInitialState: function() {
-      return { from: 0 };
+    mixins: [MouseUpdateMixin],
+    getInitialState() {
+      return { pos: 0 };
     },
-    render: function() {
-      var up = this.state.from;
-      var to = this.state.from + this.props.length;
-      var range = _.range(up, to);
-      return d.div({ "data-nav-area": ".item,#go-up,#go-down" }, _.flatten([
-        d.p({ id: "go-up", "data-nav-limit": "top", onClick: _.bind(this.scroll, this, -1) }, "go up"),
-        d.ul(null, _.map(range, function(i) { return d.li({ className: 'item', key: i }, 'item' + i); })),
-        d.p({ id: "go-down", "data-nav-limit": "bottom", onClick: _.bind(this.scroll, this, +1) }, "go down"),
-      ]));
+    scroll(dir) {
+      var pos = this.state.pos + (dir || 1);
+      var max = this.props.values.length - this.props.length;
+      if (0 <= pos && pos <= max) this.setState({ pos });
     },
-    scroll: function(dir) {
-      this.setState({ from: this.state.from + (dir || 1) });
+    render() {
+      var pos  = this.state.pos;
+      var vals = this.props.values.slice(pos, pos + this.props.length);
+      var list = _.map(vals, obj => {
+        var { key, val } = obj;
+        var selected = !!_.find(this.props.selected, { key });
+        return (
+          <li className={cx({ item: true, selected })} onClick={() => this.props.onSelect(obj)} key={key}>{val}</li>
+        )
+      });
+
+      return (
+        <div data-nav-area=".item,[data-nav-limit]">
+          <p onClick={() => this.scroll(-1)} data-nav-limit="top">go up</p>
+          <ul>{list}</ul>
+          <p onClick={() => this.scroll(+1)} data-nav-limit="bottom">go down</p>
+        </div>
+      );
     },
-    componentDidUpdate: function() {
-      mouse.update();
-    }
   });
 
-  document.addEventListener('DOMContentLoaded', function() {
-    React.renderComponent(App(), document.getElementById('main'));
-    mouse = new Mickey(document.getElementById('main'), { observer: _.noop }).init();
+  var SelectedItems = React.createClass({
+    render() {
+      return <span>{_(this.props.values).sortBy("key").pluck("val").join(', ')}</span>
+    },
   });
+
+  var values = _.range(0, 1000).map(i => ({ key: i, val: `item ${i}` }));
+  var root = document.getElementById('main');
+
+  // with react, we don't need on observer and update the mouse
+  // when components are updated.
+  var mouse = new Mickey(root, { observer: _.noop });
+  React.renderComponent(<App title="foo" values={values}/>, root);
 
 })(window.React, window.Mickey);
