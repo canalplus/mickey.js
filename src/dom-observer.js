6df530f296df655d2a037fc13c891b93b0c45f43
Pierre Guilleminot <pierre.guilleminot@gmail.com>
2014-09-11 19:31:11 +0200

    dom-observer: do not observe attributes change

--- src/dom-observer.js
+++ src/dom-observer.js
@@ -4,7 +4,7 @@ module.exports = function DOMObserver(target, fn) {
   var obs;
   if (window.MutationObserver != null) {
     obs = new MutationObserver(fn);
-    obs.observe(target, { attributes: true, childList: true, characterData: true, subtree: true });
+    obs.observe(target, { attributes: false, childList: true, characterData: true, subtree: true });
   } else {
     fn = _.debounce(fn, 0);
     target.addEventListener('DOMSubtreeModified', fn);
