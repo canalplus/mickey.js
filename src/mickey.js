b1d032ab0756c5605b70d66f67a7b12e621d92f0
Pierre Guilleminot <pierre.guilleminot@gmail.com>
2014-09-14 21:56:16 +0200

    es6 refacto (drop underscore support)

--- src/mickey.js
+++ src/mickey.js
@@ -61,72 +61,88 @@ function dataSorter(name, ord) {
   return el => el.hasAttribute("data-" + name) ? ord : 0;
 }
 
-var limitLast     = dataSorter('navLimit', 1);
+var limitLast = dataSorter('navLimit', 1);
 var selectedFirst = dataSorter('navSelected', -1);
 
+function dispatchEvent(el, {x, y}, type) {
+  if (!el) return;
+  var ev = document.createEvent('MouseEvents');
+  ev.initMouseEvent(type, true, true, window, 0, x, y, x, y, false, false, false, false, 0, null);
+  el.dispatchEvent(ev);
+}
 
-function Mickey(parent, options) {
-  if (!parent)
-    throw new Error('mickey: should pass a parent DOM element');
+function isArea(el, root) {
+  return !!el && (el.dataset.hasAttribute("data-nav-area") || el === root);
+}
+
+function isLimit(el) {
+  return !!el && el.dataset.hasAttribute("data-nav-limit");
+}
+
+function isTracked(el) {
+  return !!el && el.dataset.hasAttribute("data-nav-track");
+}
+
+function checkCircular(el, dir) {
+  if (!el || !el.hasAttribute("data-nav-circular")) return false;
+  var circular = el.dataset.navCircular;
+  return circular === '' || DIRS[dir] === circular;
+}
+
+function checkLimit(el, dir) {
+  return !!dir && isLimit(el) && el.dataset.navLimit === LIMITS[dir];
+}
+
+function isSelected(el) {
+  return !!el && el.dataset.hasAttribute("data-nav-selected");
+}
+
+// Find all the areas in the DOM.
+function allAreas(root, selector) {
+  var els = $find(root, selector);
+  return els.length ? els: [root];
+}
+
+// Find the default area: the one containing data-selected
+// attribute or (if none) the first one in the DOM.
+function defaultArea(root, selector) {
+  var els = allAreas(root, selector);
+  if (_.some(els, isSelected)) {
+    els = _.sortBy(els, selectedFirst);
+  }
+  return els[0];
+}
+
+// Find all selectable elements inside the given DOM element.
+function allSelectables(el, selector, dir) {
+  var els = $find(el, el.dataset.navArea);
+  var lim = _.some(els, isLimit);
+  if (lim) els = _.sortBy(els, limitLast);
+  if (lim && dir) {
+    return _.filter(els, el => !isLimit(el) || checkLimit(el, dir));
+  } else {
+    return els;
+  }
+}
+
+function Mickey(root, options = {}) {
+  if (!root)
+    throw new Error('mickey: should pass a root DOM element');
 
   var locked = false;
   var inited = false;
 
-  options = _.defaults(options || {}, {
+  var { $area, $href, hoverClass, areaClass, trackClass, overlap } = _.defaults(options, {
+    position: null,
     hoverClass: 'hover',
     areaClass:  'hover',
     trackClass: 'tracked',
     overlap: 0,
-    position: null,
     listener: keyListener,
     observer: DOMObserver,
     $area: '[data-nav-area]',
-    $href: null
+    $href: null,
   });
-
-  var mouse = {
-    version: '1.0.2',
-    pos: options.position || nil(),
-    el: null,
-    ar: null,
-  };
-
-  var listener = options.listener(mouse);
-
-  function dispatchEvent(el, type) {
-    if (!el) return;
-    var ev = document.createEvent('MouseEvents');
-    var x = mouse.pos.x;
-    var y = mouse.pos.y;
-    ev.initMouseEvent(type, true, true, window, 0, x, y, x, y, false, false, false, false, 0, null);
-    el.dispatchEvent(ev);
-  }
-
-  function isArea(el) {
-    return !!el && (el.dataset.hasAttribute("data-nav-area") || el === parent);
-  }
-
-  function isLimit(el) {
-    return !!el && el.dataset.hasAttribute("data-nav-limit");
-  }
-
-  function isTracked(el) {
-    return !!el && el.dataset.hasAttribute("data-nav-track");
-  }
-
-  function checkCircular(el, dir) {
-    if (!el || !el.hasAttribute("data-nav-circular")) return false;
-    var circular = el.dataset.navCircular;
-    return circular === '' || DIRS[dir] === circular;
-  }
-
-  function checkLimit(el, dir) {
-    return !!dir && isLimit(el) && el.dataset.navLimit === LIMITS[dir];
-  }
-
-  function isSelected(el) {
-    return !!el && el.dataset.hasAttribute("data-nav-selected");
-  }
 
   // Finds and returns the closest element from a given vector
   // position or Box to a set of DOM elements.
@@ -134,26 +150,26 @@ function Mickey(parent, options) {
   // If a direction is given as a vector or string 'up', 'left'
   // 'down', 'right', the closest element will be searched in the
   // halfspace defined by the direction and the position.
-  function findClosest(pos, els, dir, area) {
+  function findClosest(pos, els, dir, fromCenter = true) {
     var v  = dir ? BASE[dir] : nil();
     var v_ = opp(v);
 
     if (pos instanceof Box)
       pos = pos.bound(v);
 
-    var halfSpace = p => dot(vec(pos, p), v) >= -options.overlap;
+    var halfSpace = p => dot(vec(pos, p), v) >= -overlap;
 
-    var res = _.sortBy(_.map(_.filter(_.map(els, createBox),
-      b => b && halfSpace(area ? b.bound(v_) : b.center())),
-      b => ({
+    var res = _(els)
+      .map(createBox)
+      .filter(b => b && halfSpace(fromCenter ? b.center() : b.bound(v_)))
+      .map(b => ({
+        box:  b,
         el:   b.el,
         proj: distp(pos, b.bound(v_), v),
         dist: dist1(pos, b.bound(v_))
-      })),
-      x => x.proj);
-
-    if (res.length > 1 && res[0].proj === res[1].proj)
-      res = _.sortBy(res, x => x.dist);
+      }))
+      .sortBy(['proj', 'dist'])
+      .value();
 
     return res[0] && res[0].el;
   }
@@ -165,257 +181,243 @@ function Mickey(parent, options) {
     if (box && box.contains(pos, BASE)) return box.el;
   }
 
-  // Find all the areas in the DOM.
-  function allAreas() {
-    var els = $find(parent, options.$area);
-    return els.length ? els: [parent];
-  }
+  var mouse = {
+    version: '1.0.1',
+    pos: options.position || nil(),
+    el: null,
+    ar: null,
 
-  // Find the default area: the one containing data-selected
-  // attribute or (if none) the first one in the DOM.
-  function defaultArea() {
-    var els = allAreas();
-    if (_.some(els, isSelected)) {
-      els = _.sortBy(els, selectedFirst);
-    }
-    return _.first(els);
-  }
+    // mouse initialization
+    init() {
+      if (inited) throw new Error('mickey: already initialized');
 
-  // Find all selectable elements inside the given DOM element.
-  function allSelectables(el, dir) {
-    var els = $find(el, el.dataset.navArea || options.$href);
-    var lim = _.some(els, isLimit);
-    if (lim) els = _.sortBy(els, limitLast);
-    if (lim && dir) {
-      return _.filter(els, el => !isLimit(el) || checkLimit(el, dir));
-    } else {
-      return els;
-    }
-  }
+      bind();
+
+      mouse.focus(options.position ?
+        mouse.hovered() :
+        mouse.defaults());
+
+      return mouse;
+    },
+
+    // focus update on current area
+    update() {
+      mouse.focus(mouse.closest());
+    },
+
+    focus(el, dir, forceFallback = false) {
+      if (_.isString(el))
+        el = root.querySelector(el);
+
+      if (isArea(el, root))
+        return mouse.focus(mouse.defaults(el));
+
+      var box = createBox(el);
+      if (!box) return false;
+
+      var newEl = el;
+      var newAr = mouse.area(newEl);
+      var memEl = mouse.el;
+      var memAr = mouse.ar;
+
+      var newLimit  = isLimit(newEl);
+      var shiftArea = newAr !== memAr;
+
+      if (shiftArea) {
+        mouse.ar = newAr;
+        $rmvClass(memAr, areaClass);
+        $addClass(newAr, areaClass);
+      }
+
+      if (newEl !== memEl &&
+         (newAr !== memAr || !newLimit || forceFallback)) {
+        mouse.pos = box.center();
+        mouse.el = newEl;
+        $rmvClass(memEl, hoverClass);
+        $addClass(memEl, trackClass, shiftArea && isTracked(memAr));
+        dispatchEvent(memEl, mouse.pos, 'mouseout');
+        dispatchEvent(newEl, mouse.pos, 'mouseover');
+      }
+
+      $rmvClass(newEl, trackClass);
+      $addClass(newEl, hoverClass, !newLimit);
+
+      if (newLimit && checkLimit(newEl, dir))
+        mouse.click(el);
+
+      if (!inited) inited = true;
+
+      return true;
+    },
+
+    position() {
+      var x = mouse.pos.x;
+      var y = mouse.pos.y;
+      return { x, y };
+    },
+
+    move(dir) {
+      if (locked)
+        throw new Error('mickey: locked');
+
+      var curEl = mouse.el;
+      var boxEl = createBox(curEl);
+      if (!boxEl) {
+        if (!fallback(dir)) throw new Error('mickey: cannot move');
+        return;
+      }
+
+      // find the closest element in the same area as the current focused
+      // element
+      var curAr = mouse.area();
+      var selectables = _.without(allSelectables(curAr, $href, dir), curEl);
+      var newEl = findClosest(boxEl, selectables, dir);
+      if (newEl)
+        return mouse.focus(newEl, dir);
+
+      var zidx = +curAr.dataset.navZIndex;
+      if (zidx > 0) return;
+
+      if (checkCircular(curAr, dir))
+        return mouse.focus(mouse.circular(dir));
+
+      // if no close element has been found, we may have to search for the
+      // closest area, or check for a limit element
+      var areas = _.without(allAreas(root, $area), curAr);
+      var boxAr = createBox(curAr);
+      var newAr = findClosest(boxAr, areas, dir, true);
+      if (!newAr) {
+        return checkLimit(mouse.el, dir)
+          ? mouse.click()
+          : false;
+      }
+
+      // for a data-area containing only one limit element
+      var els = allSelectables(newAr, $href);
+      if (els.length === 1 && checkLimit(els[0], dir))
+        return mouse.click(els[0]);
+
+      if (isTracked(curAr))
+        curAr.dataset.navTrackPos = JSON.stringify(mouse.pos);
+
+      if (isTracked(newAr)) {
+        var trackPos = newAr.dataset.navTrackPos;
+        var trackElt = $first(newAr, '.' + trackClass);
+        newEl = trackElt || (trackPos && findClosest(JSON.parse(trackPos), els));
+      } else {
+        newEl = els[0];
+      }
+
+      return mouse.focus(newEl, dir);
+    },
+
+    click(el = mouse.el) {
+      if (locked || !inited)
+        throw new Error('mickey: locked');
+
+      if (!root.contains(el))
+        throw new Error('mickey: cannot click on non visible element');
+
+      if (!el && !fallback())
+        throw new Error('mickey: cannot click');
+
+      dispatchEvent(el, mouse.pos, 'click');
+      return true;
+    },
+
+    area(el = mouse.el) {
+      while (el && el !== root) {
+        el = el.parentNode;
+        if (isArea(el, root)) return el;
+      }
+      return root;
+    },
+
+    closest(ar) {
+      var ars = ar ? [ar] : allAreas(root, $area);
+      var els = _.flatten(ars, true, a => allSelectables(a, $href));
+      return findClosest(mouse.pos, els);
+    },
+
+    closestInArea() {
+      return mouse.closest(mouse.ar);
+    },
+
+    defaults(ar) {
+      return (allSelectables(ar || defaultArea(root, $area), $href))[0];
+    },
+
+    defaultsInArea() {
+      return mouse.defaults(mouse.ar);
+    },
+
+    hovered() {
+      var ars = allAreas(root, $area);
+      var els = _.flatten(ars, true, a => allSelectables(a, $href));
+      return findHovered(mouse.pos, els);
+    },
+
+    circular(dir) {
+      var center = createBox(mouse.ar).center();
+      var reflect = dir
+        ? axisReflect(createBox(mouse.el).bound(BASE[dir]), center, BASE[dir])
+        : pointReflect(mouse.pos, center);
+      var els = allSelectables(mouse.ar, $href);
+      return findClosest(reflect, els, dir);
+    },
+
+    block() {
+      locked = true;
+      $addClass(mouse.el, 'blocked');
+    },
+
+    unblock() {
+      locked = false;
+      $rmvClass(mouse.el, 'blocked');
+    },
+
+    // clear mouse
+    clear: _.once(() => {
+      unbind();
+      mouse.pos = nil();
+      mouse.el = null;
+      mouse.ar = null;
+      root = null;
+      locked = false;
+    }),
+  };
+
+  //
+  // PRIVATES
+  //
+
+  var { bind, unbind } = ((Observer, Listener) => {
+    var obs, lis = Listener(mouse);
+    return {
+      bind: _.once(() => {
+        obs = Observer(root, watch);
+        if (lis.bind) lis.bind(root);
+      }),
+      unbind: _.once(() => {
+        if (obs) obs.disconnect();
+        if (lis.unbind) lis.unbind();
+      })
+    };
+  })(
+    options.observer,
+    options.listener
+  );
+
 
   function fallback(dir) {
-    return mouse.focus(mouse.closest() || defaultArea(), dir, true);
+    return mouse.focus(mouse.closest() || defaultArea(root, $area), dir, true);
   }
 
-  var obs;
-  var bind = _.once(() => {
-    obs = options.observer(parent, watch);
-    if (listener.bind) listener.bind(parent);
-  });
-
-  var unbind = _.once(() => {
-    obs && obs.disconnect();
-    if (listener.unbind) listener.unbind();
-  });
-
-  mouse.focus = function(el, dir, fallback) {
-    if (_.isString(el)) {
-      el = parent.querySelector(el);
-    }
-
-    if (isArea(el))
-      return mouse.focus(mouse.defaults(el));
-
-    var box = createBox(el);
-    if (!box) return false;
-
-    var newEl = el;
-    var newAr = mouse.area(newEl);
-    var memEl = mouse.el;
-    var memAr = mouse.ar;
-
-    var newLimit  = isLimit(newEl);
-    var shiftArea = newAr !== memAr;
-
-    if (shiftArea) {
-      mouse.ar = newAr;
-      $rmvClass(memAr, options.areaClass);
-      $addClass(newAr, options.areaClass);
-    }
-
-    if (newEl !== memEl &&
-       (newAr !== memAr || !newLimit || fallback)) {
-      mouse.pos = box.center();
-      mouse.el = newEl;
-      $rmvClass(memEl, options.hoverClass);
-      $addClass(memEl, options.trackClass, shiftArea && isTracked(memAr));
-      dispatchEvent(memEl, 'mouseout');
-      dispatchEvent(newEl, 'mouseover');
-    }
-
-    $rmvClass(newEl, options.trackClass);
-    $addClass(newEl, options.hoverClass, !newLimit);
-
-    if (newLimit && checkLimit(newEl, dir)) {
-      mouse.click(el);
-    }
-
-    if (!inited) inited = true;
-
-    return true;
-  };
-
-  mouse.position = function() {
-    return {
-      x: mouse.pos.x,
-      y: mouse.pos.y
-    };
-  };
-
-  mouse.move = function(dir) {
-    if (locked)
-      throw new Error('mickey: locked');
-
-    var curEl = mouse.el;
-    var boxEl = createBox(curEl);
-    if (!boxEl) {
-      if (!fallback(dir)) throw new Error('mickey: cannot move');
-      return;
-    }
-
-    // find the closest element in the same area as the current focused
-    // element
-    var curAr = mouse.area();
-    var selectables = _.without(allSelectables(curAr, dir), curEl);
-    var newEl = findClosest(boxEl, selectables, dir);
-    if (newEl)
-      return mouse.focus(newEl, dir);
-
-    var zidx = +curAr.dataset.navZIndex;
-    if (zidx > 0)
-      return;
-
-    if (checkCircular(curAr, dir))
-      return mouse.focus(mouse.circular(dir));
-
-    // if no close element has been found, we may have to search for the
-    // closest area, or check for a limit element
-    var boxAr = createBox(curAr);
-    var areas = _.without(allAreas(), curAr);
-    var newAr = findClosest(boxAr, areas, dir, true);
-    if (!newAr) {
-      if (checkLimit(mouse.el, dir))
-        return mouse.click();
-      else
-        return false;
-    }
-
-    // for a data-area containing only one limit element
-    var els = allSelectables(newAr);
-    if (els.length === 1 && checkLimit(els[0], dir))
-      return mouse.click(els[0]);
-
-    if (isTracked(curAr)) {
-      curAr.dataset.navTrackPos = JSON.stringify(mouse.pos);
-    }
-
-    if (isTracked(newAr)) {
-      var trackPos = newAr.dataset.navTrackPos;
-      var trackElt = $first(newAr, '.' + options.trackClass);
-      newEl = trackElt || (trackPos && findClosest(JSON.parse(trackPos), els));
-    }
-
-    return mouse.focus(newEl || els[0], dir);
-  };
-
-  mouse.click = function(el) {
-    if (locked || !inited) throw new Error('mickey: locked');
-    el = el || mouse.el;
-    if (!parent.contains(el, BASE)) throw new Error('mickey: cannot click on non visible element');
-    if (!el && !fallback())         throw new Error('mickey: cannot click');
-    dispatchEvent(el, 'click');
-    return true;
-  };
-
-  // current mouse area
-  mouse.area = function(el) {
-    el = el || mouse.el;
-    while (el && el !== parent) {
-      el = el.parentNode;
-      if (isArea(el)) return el;
-    }
-    return parent;
-  };
-
-  mouse.closest = function(ar) {
-    var els = _.flatten(_.map(ar ? [ar] : allAreas(), allSelectables));
-    return findClosest(mouse.pos, els);
-  };
-
-  mouse.closestInArea = function() {
-    return mouse.closest(mouse.ar);
-  };
-
-  mouse.defaults = function(ar) {
-    return (allSelectables(ar || defaultArea()))[0];
-  };
-
-  mouse.defaultsInArea = function() {
-    return mouse.defaults(mouse.ar);
-  };
-
-  mouse.hovered = function() {
-    var els = _.flatten(_.map(allAreas(), allSelectables));
-    return findHovered(mouse.pos, els);
-  };
-
-  mouse.circular = function(dir) {
-    var reflect;
-    var center = createBox(mouse.ar).center();
-    if (dir) {
-      reflect = axisReflect(createBox(mouse.el).bound(BASE[dir]), center, BASE[dir]);
-    } else {
-      reflect = pointReflect(mouse.pos, center);
-    }
-    return findClosest(reflect, allSelectables(mouse.ar), dir);
-  };
-
-  mouse.block = function() {
-    locked = true;
-    $addClass(mouse.el, 'blocked');
-  };
-
-  mouse.unblock = function() {
-    locked = false;
-    $rmvClass(mouse.el, 'blocked');
-  };
-
-  // clear mouse
-  mouse.clear = _.once(() => {
-    unbind();
-    mouse.pos = nil();
-    mouse.el = null;
-    mouse.ar = null;
-    parent = null;
-    locked = false;
-    listener = null;
-  });
-
-  // focus update on current area
-  mouse.update = function() {
-    mouse.focus(mouse.closest());
-  };
-
-  // mouse initialization
-  mouse.init = function() {
-    if (inited) {
-      throw new Error('mickey: already initialized');
-    }
-
-    bind();
-
-    mouse.focus(options.position ?
-      mouse.hovered() :
-      mouse.defaults());
-
-    return mouse;
-  };
-
-  var watch = function() {
+  function watch() {
     if (!inited)
       return mouse.init();
 
-    if (!parent || parent.contains(mouse.el, BASE))
+    if (!root || root.contains(mouse.el))
       return;
 
     // TODO: handle mouse.ar disapearance ?
@@ -429,7 +431,7 @@ function Mickey(parent, options) {
     }
 
     mouse.focus(el, null, true);
-  };
+  }
 
   return mouse;
 }
